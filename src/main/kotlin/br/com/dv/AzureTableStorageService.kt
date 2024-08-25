db67a712de683b8186a705b8f5012780e5b9c455
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-08-25 19:00:29 -0300

    Refactor AzureTableStorageService: extract method, use apply scoping function

--- src/main/kotlin/br/com/dv/AzureTableStorageService.kt
+++ src/main/kotlin/br/com/dv/AzureTableStorageService.kt
@@ -8,30 +8,27 @@ import org.springframework.stereotype.Service
 class AzureTableStorageService(private val tableClient: TableClient) {
 
     fun saveEntity(data: YouthExchange): TableEntity {
-        val partitionKey = data.partitionKey
-        val rowKey = data.rowKey
-
-        val entity = TableEntity(partitionKey, rowKey)
-            .addProperty("playerName", data.name)
-            .addProperty("age", data.age)
-            .addProperty("nationality", data.country)
-            .addProperty("hp", data.hp)
-            .addProperty("firstHpSkill", data.firstHpSkill)
-            .addProperty("secondHpSkill", data.secondHpSkill)
-            .addProperty("lp", data.lp)
-            .addProperty("firstLpSkill", data.firstLpSkill)
-            .addProperty("secondLpSkill", data.secondLpSkill)
-            .addProperty("trainingSpeed", data.trainingSpeed)
-            .addProperty("totalSkillBalls", data.totalBalls)
-            .addProperty("owner", data.owner)
-
-        data.stats.forEach { (key, value) ->
-            entity.addProperty(key, value)
-        }
-
+        val entity = createTableEntity(data)
         tableClient.upsertEntity(entity)
-
         return entity
     }
 
+    private fun createTableEntity(data: YouthExchange): TableEntity {
+        return TableEntity(data.partitionKey, data.rowKey).apply {
+            addProperty("playerName", data.name)
+            addProperty("age", data.age)
+            addProperty("nationality", data.country)
+            addProperty("hp", data.hp)
+            addProperty("firstHpSkill", data.firstHpSkill)
+            addProperty("secondHpSkill", data.secondHpSkill)
+            addProperty("lp", data.lp)
+            addProperty("firstLpSkill", data.firstLpSkill)
+            addProperty("secondLpSkill", data.secondLpSkill)
+            addProperty("trainingSpeed", data.trainingSpeed)
+            addProperty("totalSkillBalls", data.totalBalls)
+            addProperty("owner", data.owner)
+            data.stats.forEach { (key, value) -> addProperty(key, value) }
+        }
+    }
+    
 }
