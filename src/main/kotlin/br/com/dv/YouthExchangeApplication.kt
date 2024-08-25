51dfa18cbc7fab2daa3bbca633165c9854409a66
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-08-25 16:43:21 -0300

    Initial commit: set up Spring Boot project

---
+++ src/main/kotlin/br/com/dv/YouthExchangeApplication.kt
@@ -0,0 +1,11 @@
+package br.com.dv
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YouthExchangeApplication
+
+fun main(args: Array<String>) {
+	runApplication<YouthExchangeApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/dv/YouthExchangeApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.dv
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YouthExchangeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
